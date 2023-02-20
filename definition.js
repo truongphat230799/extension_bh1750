40879baa163ae689371797abafaa3d345749ede1
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2023-02-20 17:52:48 +0700

    Update definition.js

--- definition.js
+++ definition.js
@@ -16,10 +16,10 @@ Blockly.Blocks['yolobit_bh1750_setup'] = {
 Blockly.Python['yolobit_bh1750_setup'] = function(block) {
     Blockly.Python.definitions_['import_yolobit'] = 'from yolobit import *';
     Blockly.Python.definitions_['import_i2c'] = 'from machine import Pin, SoftI2C';
-    Blockly.Python.definitions_["import_bh1750"] = "import bh1750";
+    Blockly.Python.definitions_["import_bh1750"] = "from bh1750 import BH1750";
     Blockly.Python.definitions_["import_create_mlx90614"] = "s = BH1750(SoftI2C(scl=Pin(22), sda=Pin(21), freq =100000))";
     // TODO: Assemble Python into code variable.
-    var code = 's.luminance(BH1750.ONCE_HIRES_1)';
+    var code = 'round(s.luminance(BH1750.ONCE_HIRES_1),1)';
     // TODO: Change ORDER_NONE to the correct strength.
     return [code, Blockly.Python.ORDER_NONE];
   };
